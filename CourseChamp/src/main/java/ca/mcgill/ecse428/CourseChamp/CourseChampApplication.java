00966a64268f29a7435c54fbc4f9437be7fd33f1
Walid Aissa <walidaissa047@gmail.com>
2024-02-08 21:28:03 -0500

    Spring and Vue.js file structure generation

---
+++ CourseChamp/src/main/java/ca/mcgill/ecse428/CourseChamp/CourseChampApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.CourseChamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseChampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseChampApplication.class, args);
+	}
+
+}
