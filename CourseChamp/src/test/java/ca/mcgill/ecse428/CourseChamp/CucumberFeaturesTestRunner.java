f2f0a29bd3d00d7f2655139128e3f131082dc2ec
Emilien-T <112722759+Emilien-T@users.noreply.github.com>
2024-02-10 11:43:30 -0500

    setup gradle cucumber tests

---
+++ CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/CucumberFeaturesTestRunner.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse428.CourseChamp;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", features = "src/test/resources/",
+        glue = "ca.mcgill.ecse428.CourseChamp")
+public class CucumberFeaturesTestRunner {
+}
+
---
+++ CourseChamp/src/test/resources/features/account_credited.feature
@@ -0,0 +1,4 @@
+Feature: Account is credited with amount
+
+  Scenario: Credit amount
+    Given account balance is 0.0
