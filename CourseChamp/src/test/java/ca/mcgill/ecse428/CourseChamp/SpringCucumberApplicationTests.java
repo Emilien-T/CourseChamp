ea96519ac65e78baaaf5380ae23879da01ce7f58
Kalamar136 <karl.bridi@mail.mcgill.ca>
2024-02-13 21:12:21 -0500

    Minor changes

--- CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/SpringCucumberApplicationTests.java
+++ CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/SpringCucumberApplicationTests.java
@@ -1,11 +1,14 @@
 package ca.mcgill.ecse428.CourseChamp;
 
 import io.cucumber.spring.CucumberContextConfiguration;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
 @SpringBootTest
 class SpringCucumberApplicationTests {
+    @Test
     void contextLoads() {
     }
 
