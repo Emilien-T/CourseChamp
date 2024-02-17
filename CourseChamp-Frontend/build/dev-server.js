4d951bfa803e39955400bab0de923789e703d8d6
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-02-17 13:38:23 -0500

    Change dev-server.js to match 321 tutorial

--- CourseChamp-Frontend/build/dev-server.js
+++ CourseChamp-Frontend/build/dev-server.js
@@ -65,7 +65,9 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+//var uri = 'http://localhost:' + port
+var host = config.dev.host
+var uri = 'http://' + host + ':' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
