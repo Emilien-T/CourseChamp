54ba919f901cd4c536295d679f4f08c8c27768e5
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-02-17 11:19:35 -0500

    Add backend path to config/index.js

--- CourseChamp-Frontend/config/index.js
+++ CourseChamp-Frontend/config/index.js
@@ -4,6 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
+    port: 8087,
+    backendBaseUrl: 'http://localhost:8080',
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
@@ -22,8 +24,9 @@ module.exports = {
     bundleAnalyzerReport: process.env.npm_config_report
   },
   dev: {
-    env: require('./dev.env'),
-    port: 8080,
+    env: require('./prod.env'),
+    port: 8087,
+    backendBaseUrl: 'http://localhost:8080',
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
