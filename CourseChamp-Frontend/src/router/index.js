7494d30f58d4f034b19500f94be274df1f0079ab
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-02-17 12:58:09 -0500

    Add basic signup page

--- CourseChamp-Frontend/src/router/index.js
+++ CourseChamp-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import SignUp from '@/components/SignUp'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
