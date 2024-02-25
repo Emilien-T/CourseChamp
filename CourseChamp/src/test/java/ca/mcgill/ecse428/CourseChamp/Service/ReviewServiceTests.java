b68abda67408ad4eeede9115aac2a462ea7f6cb0
Ad2Am2 <92311639+Ad2Am2@users.noreply.github.com>
2024-02-25 16:25:44 -0500

    More tests

--- CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/Service/ReviewServiceTests.java
+++ CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/Service/ReviewServiceTests.java
@@ -84,5 +84,103 @@ public class ReviewServiceTests {
         });
     }
 
+    @Test
+    public void testCreateReviewNullText() {
+        assertThrows(CourseChampException.class, () -> {
+            Review review = new Review();
+            CourseOffering courseOffering = new CourseOffering();
+            review.setCourseOffering(courseOffering);
+            reviewService.createReview(review);
+        });
+    }
+
+    @Test
+    public void testCreateReviewNullRating() {
+        assertThrows(CourseChampException.class, () -> {
+            Review review = new Review();
+            CourseOffering courseOffering = new CourseOffering();
+            review.setCourseOffering(courseOffering);
+            review.setText("This is a review");
+            reviewService.createReview(review);
+        });
+    }
+
+
+    @Test
+    public void testGetReview() {
+        int reviewId = 1;
+        CourseOffering courseOffering = new CourseOffering();        
+        String reviewText = "This is a review";
+        int rating = 5;
+
+        Review review = new Review();
+        review.setId(reviewId);
+        review.setCourseOffering(courseOffering);
+        review.setText(reviewText);
+        review.setRating(rating);
+
+        when(reviewRepository.findById(reviewId)).
+            thenReturn(java.util.Optional.of(review));
+
+        Review foundReview = reviewService.getReviewById(reviewId);
+
+        assertNotNull(foundReview);
+        assertEquals(reviewId, foundReview.getId());
+        assertEquals(courseOffering, foundReview.getCourseOffering());
+        assertEquals(reviewText, foundReview.getText());
+        assertEquals(rating, foundReview.getRating());
+
+    }
+
+    @Test
+    public void testGetReviewNotFound() {
+        int reviewId = 1;
+
+        when(reviewRepository.findById(reviewId)).
+            thenReturn(java.util.Optional.empty());
+
+        assertThrows(CourseChampException.class, () -> {
+            reviewService.getReviewById(reviewId);
+        });
+    }
+
+
+    @Test
+    public void testFindReviewsByCourseCode() {
+        int reviewId = 1;
+        CourseOffering courseOffering = new CourseOffering();        
+        String reviewText = "This is a review";
+        int rating = 5;
+
+        Review review = new Review();
+        review.setId(reviewId);
+        review.setCourseOffering(courseOffering);
+        review.setText(reviewText);
+        review.setRating(rating);
+
+        when(reviewRepository.findReviewsByCourseCode("ECSE428")).
+            thenReturn(java.util.List.of(review));
+
+        Review foundReview = reviewService.findReviewsByCourseCode("ECSE428").get(0);
+
+        assertNotNull(foundReview);
+        assertEquals(reviewId, foundReview.getId());
+        assertEquals(courseOffering, foundReview.getCourseOffering());
+        assertEquals(reviewText, foundReview.getText());
+        assertEquals(rating, foundReview.getRating());
+
+    }
+
+
+    @Test
+    public void testFindReviewsByCourseCodeNotFound() {
+        when(reviewRepository.findReviewsByCourseCode("ECSE428")).
+            thenReturn(java.util.List.of());
+
+        assertThrows(CourseChampException.class, () -> {
+            reviewService.findReviewsByCourseCode("ECSE428");
+        });
+    }
+
     
 }
