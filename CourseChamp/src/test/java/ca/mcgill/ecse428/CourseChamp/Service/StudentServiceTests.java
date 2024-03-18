5557da756601a1cbdc0e435f7dd3bd30274f0350
Emilien-T <112722759+Emilien-T@users.noreply.github.com>
2024-03-18 16:48:19 -0400

    finished the service tests for user portal

--- CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/Service/StudentServiceTests.java
+++ CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/Service/StudentServiceTests.java
@@ -1,9 +1,13 @@
 package ca.mcgill.ecse428.CourseChamp.Service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,9 +17,13 @@ import org.springframework.http.HttpStatus;
 
 import ca.mcgill.ecse428.CourseChamp.exception.CourseChampException;
 import ca.mcgill.ecse428.CourseChamp.model.Admin;
+import ca.mcgill.ecse428.CourseChamp.model.Course;
+import ca.mcgill.ecse428.CourseChamp.model.CourseOffering;
+import ca.mcgill.ecse428.CourseChamp.model.Review;
 import ca.mcgill.ecse428.CourseChamp.model.Student;
 import ca.mcgill.ecse428.CourseChamp.model.Student.Major;
 import ca.mcgill.ecse428.CourseChamp.repository.AdminRepository;
+import ca.mcgill.ecse428.CourseChamp.repository.ReviewRepository;
 import ca.mcgill.ecse428.CourseChamp.repository.StudentRepository;
 import ca.mcgill.ecse428.CourseChamp.service.StudentService;
 
@@ -26,6 +34,8 @@ public class StudentServiceTests {
     private StudentRepository studentRepository;
     @Mock
     private AdminRepository adminRepository;
+    @Mock
+    private ReviewRepository reviewRepository;
 
     @InjectMocks
     private StudentService studentService;
@@ -145,8 +155,7 @@ public class StudentServiceTests {
 
         // when(studentRepository.save(john)).thenReturn(john);
 
-        // CourseChampException e = assertThrows(CourseChampException.class,
-        // () -> studentService.createStudentAccount(john));
+        // CourseChampException e = assertThrows(CourseChampException.class, () -> studentService.createStudentAccount(john));
         // assertEquals(e.getStatus(), HttpStatus.BAD_REQUEST);
         // assertEquals(e.getMessage(), "All fields must be filled");
     }
@@ -328,4 +337,124 @@ public class StudentServiceTests {
     }
 
     // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=//
+
+    // =-=-=-=-=-=-= Update Student Service Tests =-=-=-=-=-=//
+
+    // Update all feilds successfully
+    @Test
+    public void testUpdateStudentSuccessfully() {
+        final String email = "john.doe@mcgill.ca";
+        final String username = "John Doe";
+        final String password = "JohnDoe2002";
+        final Major major = Major.Software;
+        final Student john = new Student(email, username, password, major);
+
+        when(studentRepository.findStudentByEmail(email)).thenReturn(john);
+
+        final String newUsername = "Jordan Doe";
+        final String newPassword = "JordanDoe2003";
+        final Major newMajor = Major.Computer;
+        final Student newJohn = new Student(email, newUsername, newPassword, newMajor);
+
+        when(studentRepository.save(john)).thenReturn(newJohn);
+
+        Student output = studentService.updateStudentAccount(newJohn);
+
+        assertNotNull(output);
+        assertEquals(newJohn, output);
+    }
+
+    // Try to update email (should be unsucessfull)
+    @Test
+    public void testUpdateStudentEmail() {
+        final String email = "john.doe@mcgill.ca";
+        final String username = "John Doe";
+        final String password = "JohnDoe2002";
+        final Major major = Major.Software;
+        final Student john = new Student(email, username, password, major);
+
+        final String newEmail = "john.doe@mcgill.ca";
+        final String newUsername = "";
+        final String newPassword = "JordanDoe2003";
+        final Major newMajor = Major.Computer;
+        final Student newJohn = new Student(newEmail, newUsername, newPassword, newMajor);
+
+        when(studentRepository.findStudentByEmail(newEmail)).thenReturn(null);
+
+        CourseChampException e = assertThrows(CourseChampException.class,
+                () -> studentService.updateStudentAccount(newJohn));
+        assertEquals(e.getStatus(), HttpStatus.NOT_FOUND);
+        assertEquals(e.getMessage(), "Student account not found");
+    }
+    
+
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
+
+    // =-=-=-=-= Get Student Reviews Service Tests =-=-=-= //
+
+    // Get reviews of the student
+    // Try to update email (should be unsucessfull)
+    @Test
+    public void testGetReviewsSuccessfully() {
+        //Creating student
+        final String email = "john.doe@mcgill.ca";
+        final String username = "John Doe";
+        final String password = "JohnDoe2002";
+        final Major major = Major.Software;
+        final Student john = new Student(email, username, password, major);
+
+        when(studentRepository.findStudentByEmail(email)).thenReturn(john);
+
+        //Creating course offering
+        CourseOffering courseOffering = new CourseOffering();
+        Course course = new Course();
+        String courseCode = "ECSE428";
+        course.setCourseCode(courseCode);
+        courseOffering.setCourse(course);
+        
+        //Creating Review
+        ArrayList<Review> mockReviews = new ArrayList<>();
+        int reviewId = 1;
+        String reviewText = "This is a review";
+        int rating = 5;
+
+        Review review = new Review();
+        review.setId(reviewId);
+        review.setCourseOffering(courseOffering);
+        review.setText(reviewText);
+        review.setRating(rating);
+        review.setStudent(john);
+
+        mockReviews.add(review);
+
+        when(reviewRepository.findAll()).thenReturn(mockReviews);
+
+        List<Review> output = studentService.getReviewsOfStudent(email);
+
+        assertNotNull(output, "The returned review list should not be null.");
+        assertFalse(output.isEmpty(), "The review list should not be empty.");
+        assertEquals(mockReviews.size(), output.size(),
+                "The size of returned review list should match the mock list.");
+    }
+
+    // Student has no reviews
+    @Test
+    public void testGetReviewsUnsuccessfully() {
+        final String email = "john.doe@mcgill.ca";
+
+        ArrayList<Review> mockReviews = new ArrayList<>();
+
+        when(reviewRepository.findAll()).thenReturn(mockReviews);
+
+        Exception exception = assertThrows(
+                CourseChampException.class,
+                () -> studentService.getReviewsOfStudent(email));
+        assertEquals(HttpStatus.NOT_FOUND, ((CourseChampException) exception).getStatus(),
+                "HttpStatus should be NOT_FOUND.");
+        assertEquals("No reviews found for this student.", exception.getMessage(),
+                "Exception message should match the expected text.");
+    }
+
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= //
+
 }
