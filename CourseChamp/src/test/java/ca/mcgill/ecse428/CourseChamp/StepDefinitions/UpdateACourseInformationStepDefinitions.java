f22beb78ecf28d36947d080bf7a7952d6b420be7
Evan Boyd <evan.boyd@mail.mcgill.ca>
2024-03-25 15:20:35 -0400

    Fix update course gherkin tests

--- CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/StepDefinitions/UpdateACourseInformationStepDefinitions.java
+++ CourseChamp/src/test/java/ca/mcgill/ecse428/CourseChamp/StepDefinitions/UpdateACourseInformationStepDefinitions.java
@@ -1,27 +1,15 @@
 package ca.mcgill.ecse428.CourseChamp.StepDefinitions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.description;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpEntity;
 
-import ca.mcgill.ecse428.CourseChamp.controller.AdminController;
-import ca.mcgill.ecse428.CourseChamp.controller.CourseController;
-import ca.mcgill.ecse428.CourseChamp.dto.AdminResponseDto;
-import ca.mcgill.ecse428.CourseChamp.dto.CourseRequestDto;
-import ca.mcgill.ecse428.CourseChamp.dto.CourseResponseDto;
-import ca.mcgill.ecse428.CourseChamp.dto.ReviewRequestDto;
 import ca.mcgill.ecse428.CourseChamp.repository.AdminRepository;
+import ca.mcgill.ecse428.CourseChamp.dto.CourseResponseDto;
+import ca.mcgill.ecse428.CourseChamp.dto.CourseRequestDto;
 import ca.mcgill.ecse428.CourseChamp.repository.CourseRepository;
 import ca.mcgill.ecse428.CourseChamp.model.Course;
 import io.cucumber.java.en.Given;
@@ -36,8 +24,6 @@ import java.util.*;
 public class UpdateACourseInformationStepDefinitions {
     @Autowired
     private CourseRepository courseRepository;
-    @Autowired
-    CourseController courseController;
     @Autowired
     private TestRestTemplate client;
 
@@ -59,60 +45,47 @@ public class UpdateACourseInformationStepDefinitions {
   }
 
     @When("the admin attempts to update the course {string}, with name {string}, description {string}, and syllabus {string}")
-    public void the_admin_attempts_to_update_the_course_with_name_description_and_syllabus(String string,
-            String string2, String string3, String string4) {
-        CourseRequestDto courseRequest = new CourseRequestDto();
-        courseRequest.setName(string2);
-        courseRequest.setDescription(string3);
-        courseRequest.setSyllabus(string4);
+    public void the_admin_attempts_to_update_the_course_with_name_description_and_syllabus(String courseCode, String name, String description, String syllabus) {
+        // Create a course request DTO
+        CourseRequestDto requestDto = new CourseRequestDto();
+        requestDto.setDepartment(courseCode.substring(0, 4));
+        requestDto.setCourseNumber(Integer.parseInt(courseCode.substring(4)));
+        requestDto.setName(name);
+        requestDto.setDescription(description);
+        requestDto.setSyllabus(syllabus);
 
-        HttpEntity<CourseRequestDto> requestEntity = new HttpEntity<>(courseRequest);
-        response = client.exchange("/course/" + string, HttpMethod.PUT, requestEntity, CourseResponseDto.class);
+        // response = client.exchange("/course/" + courseCode + "/update/", HttpMethod.PUT, new HttpEntity<>(requestDto), CourseResponseDto.class);
+        
+        // Make a request to update the course
+        try{
+            response = client.exchange("/course/" + courseCode + "/update/", HttpMethod.PUT, new HttpEntity<>(requestDto), CourseResponseDto.class);
+        }
+        catch(Exception e){
+            responseError = client.exchange("/course/" + courseCode + "/update/", HttpMethod.PUT, new HttpEntity<>(requestDto), String.class);
+        }
+       
     }
-
-    @When("the admin unsuccessfully attempts to update the course {string}, with name {string}, description {string}, and syllabus {string}")
-    public void the_admin_unsuccessfully_attempts_to_update_the_course_with_name_description_and_syllabus(String string,
-            String string2, String string3, String string4) {
-        CourseRequestDto courseRequest = new CourseRequestDto();
-        courseRequest.setName(string2);
-        courseRequest.setDescription(string3);
-        courseRequest.setSyllabus(string4);
-
-        HttpEntity<CourseRequestDto> requestEntity = new HttpEntity<>(courseRequest);
-        responseError = client.exchange("/course/" + string, HttpMethod.PUT, requestEntity, String.class);
-    }
-
     @Then("the system should confirm the successful update")
     public void the_system_should_confirm_the_successful_update() {
         // Write code here that turns the phrase above into concrete actions
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        
     }
-
     @Then("the course with course code {string} should have the name {string}, description {string}, and syllabus {string}")
-    public void the_course_with_course_code_should_have_the_name_description_and_syllabus(String string, String string2,
-            String string3, String string4) {
-        CourseResponseDto updatedCourse = response.getBody();
-        assertNotNull(updatedCourse);
-        assertEquals(string2, updatedCourse.getName());
-        assertEquals(string3, updatedCourse.getDescription());
-        assertEquals(string4, updatedCourse.getSyllabus());
+    public void the_course_with_course_code_should_have_the_name_description_and_syllabus(String courseCode, String expectedName, String expectedDescription, String expectedSyllabus) {
+        // Retrieve the course details from your database or service
+        Course course = courseRepository.findCourseByCourseCode(courseCode);
+
+        // Compare the retrieved course details with the expected values
+        assertEquals(expectedName, course.getName());
+        assertEquals(expectedDescription, course.getDescription());
+        assertEquals(expectedSyllabus, course.getSyllabus());
     }
 
     @Then("the system should display the error message for unsuccessful update of a course's information {string}")
     public void the_system_should_display_the_error_message_for_unsuccessful_update_of_a_course_s_information(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        assertEquals(string.trim(), responseError.getBody());
+        assertTrue(responseError.getBody().contains(string)); 
     }
 
-    @SuppressWarnings("null")
-    @Then("the course with course code {string} should keep the name {string}, description {string}, and syllabus {string}")
-    public void the_course_with_course_code_should_keep_the_name_description_and_syllabus(String string, String string2,
-            String string3, String string4) {
-        ResponseEntity<CourseResponseDto> course = (ResponseEntity<CourseResponseDto>)courseController.getCourseByCode("/course/"+string);
-        assertNotNull(course.getBody());
-        assertEquals(string2, course.getBody().getName());
-        assertEquals(string3, course.getBody().getDescription());
-        assertEquals(string4, course.getBody().getSyllabus());
-    }
-
+    
 }
